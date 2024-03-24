c7e2238997c63761f7510a93bbd935067928f74e
John Obla <jo.github@outlook.com>
2024-03-24 13:05:39 +0000

    chore: indent plugins correctly

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,19 +1,12 @@
 -- plugins install
 return {
   {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.6',
-    dependencies = {
-      'nvim-lua/plenary.nvim'
-    }
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.6",
+    dependencies = {"nvim-lua/plenary.nvim"}
   },
-  {
-    'nvim-telescope/telescope-fzf-native.nvim',
-    build = 'make'
-  },
-  {
-    'rose-pine/neovim', name = 'rose-pine'
-  },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+  {"nvim-telescope/telescope-fzf-native.nvim",build = "make"},
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+  {"rose-pine/neovim", name = "rose-pine"}
 }
 
