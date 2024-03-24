56c5275259962242c0b31e9f3bbafea06ef2e93a
John Obla <jo.github@outlook.com>
2024-03-24 15:31:21 +0000

    feat: add language server

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -7,6 +7,14 @@ return {
   },
   {"nvim-telescope/telescope-fzf-native.nvim",build = "make"},
   {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-  {"rose-pine/neovim", name = "rose-pine"}
+  {"rose-pine/neovim", name = "rose-pine"},
+  -- lsp via lsp zero
+  {'williamboman/mason.nvim'},
+  {'williamboman/mason-lspconfig.nvim'},
+  {"VonHeikemen/lsp-zero.nvim", branch = "v3.x"},
+  {"neovim/nvim-lspconfig"},
+  {"hrsh7th/cmp-nvim-lsp"},
+  {"hrsh7th/nvim-cmp"},
+  {"L3MON4D3/LuaSnip"},
 }
 
