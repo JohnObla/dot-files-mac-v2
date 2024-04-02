58eebde59957ab136610dfe270ff83601d981977
John Obla <jo.github@outlook.com>
2024-04-02 20:53:21 +0100

    feat: search through hidden files

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -1,16 +1,38 @@
 -- https://github.com/nvim-telescope/telescope-fzf-native.nvim?tab=readme-ov-file#telescope-setup-and-configuration
 require('telescope').setup {
-  -- pickers = {
-  --   find_files = {
-  --     hidden = true
-  --   }
-  -- }
+  defaults = {
+    vimgrep_arguments = {
+      'rg',
+      '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      '--smart-case',
+      '--hidden',
+      '--ignore',
+      '--no-ignore-vcs',
+      '--glob=!node_modules/**',
+      '--glob=!.git/**',
+    },
+  }
 }
 require('telescope').load_extension('fzf')
 
 -- https://www.youtube.com/watch?v=w7i4amO_zaE
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+local find_command = {
+  'rg',
+  '--files',
+  '--ignore',
+  '--hidden',
+  '--no-ignore-vcs',
+  '-g',
+  '!node_modules/**',
+  '-g',
+  '!.git/**',
+}
+vim.keymap.set('n', '<leader>ff', function () builtin.find_files({find_command = find_command}) end, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fr', function()
   builtin.grep_string({ search = vim.fn.input("Grep > ")});
