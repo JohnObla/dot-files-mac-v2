97888c8692eb5254b95533d2b994cb1d2be0d6d3
John Obla <45395888+JohnObla@users.noreply.github.com>
2025-03-01 16:02:19 +0000

    Update config.fish: change method based on intel chip

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,15 +4,18 @@ set -gx EDITOR nvim
 # set long path for prompt
 set -g theme_short_path no
 
-# add homebrew to path
-fish_add_path /opt/homebrew/bin
+# add homebrew to path based on architecture
+if test (uname -m) = "arm64"
+    fish_add_path /opt/homebrew/bin
+    set -gx SHELL /opt/homebrew/bin/fish
+else
+    fish_add_path /usr/local/bin
+    set -gx SHELL /usr/local/bin/fish
+end
 
 # create n location and add to path
 set -gx N_PREFIX $HOME/n
 fish_add_path $N_PREFIX/bin
-
-# set fish as default shell
-set -gx SHELL /opt/homebrew/bin/fish
 
 # set config home
 set -gx XDG_CONFIG_HOME $HOME/.config
