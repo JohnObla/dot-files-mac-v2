7e3a84919e560cc728d114652dea2fcc59d01d70
John Obla <45395888+JohnObla@users.noreply.github.com>
2025-03-01 16:04:31 +0000

    Update wezterm.lua: change method based on intel chip

--- wezterm.lua
+++ wezterm.lua
@@ -3,19 +3,26 @@ local wezterm = require 'wezterm'
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
--- choose fish as default shell, with startup fish prompt
-config.default_prog = { '/opt/homebrew/bin/fish', '--login' }
--- choose font with ligatures
+
+-- Detect architecture and set the correct fish shell path
+local shell_path = '/usr/local/bin/fish' -- Default for Intel Macs
+if wezterm.target_triple:find('aarch64') then
+  shell_path = '/opt/homebrew/bin/fish' -- Use Homebrew path for Apple Silicon
+end
+
+-- Choose fish as the default shell, with startup fish prompt
+config.default_prog = { shell_path, '--login' }
+
+-- Choose font with ligatures
 config.font = wezterm.font 'JetBrains Mono'
--- remove black bar from bottom of screen
+
+-- Remove black bar from bottom of screen
 config.window_padding = {
   bottom = 0,
 }
 
-
 config.send_composed_key_when_left_alt_is_pressed = true
 config.send_composed_key_when_right_alt_is_pressed = true
 
-
--- and finally, return the configuration to wezterm
+-- And finally, return the configuration to WezTerm
 return config
