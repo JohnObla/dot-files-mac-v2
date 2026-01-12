d74b962918287a118c5ec1f11ff8daa63f1d196a
John Obla <jo.github@outlook.com>
2026-01-12 17:18:30 +0000

    feat: add direnv to fish source

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -22,3 +22,8 @@ set -gx XDG_CONFIG_HOME $HOME/.config
 
 # Added by `rbenv init` on Fri  9 May 2025 23:50:30 BST
 status --is-interactive; and rbenv init - --no-rehash fish | source
+
+# add to end of fish config Add a 301 redirect so users in Canada are served fr-CA, rather than the fr-FR that they're being served now?
+# hooks direnv into shell
+direnv hook fish | source
+
